7707f2905986a2360afbd59cf3d3680dd510b5e9
alexbowen <alexbowen@Alexs-MacBook-Pro.local>
2019-12-12 16:40:53 +0000

    add api key to get request header

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const http = require('http')
- 
+
 module.exports.loadFromFile = filePath => {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
         return resolve(JSON.parse(fs.readFileSync(filePath, 'utf8')))
     })
 }
@@ -10,19 +10,26 @@ module.exports.loadFromFile = filePath => {
 module.exports.saveToFile = (filePath, jsonData) => fs.writeFileSync(filePath, JSON.stringify(jsonData))
 
 module.exports.loadFromUrl = url => {
-    return new Promise((resolve,reject) => {
-    http.get(url, (res) => {
-        let body = ''
-    
-        res.on('data', (chunk) => {
-            body += chunk
-        });
-    
-        res.on('end', () => {
-            resolve(JSON.parse(body))
+
+    const SUBMITTER_API_KEY = process.env.SUBMITTER_API_KEY || 'development'
+    const options = {
+        headers: {
+            'Authorization': `Basic ${SUBMITTER_API_KEY}`
+        }
+    }
+    return new Promise((resolve, reject) => {
+        http.get(url, options, (res) => {
+            let body = ''
+
+            res.on('data', (chunk) => {
+                body += chunk
+            });
+
+            res.on('end', () => {
+                resolve(JSON.parse(body))
+            })
+        }).on('error', (e) => {
+            reject(e)
         })
-    }).on('error', (e) => {
-        reject(e)
     })
-})
 }
